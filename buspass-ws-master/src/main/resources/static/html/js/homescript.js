4a5e87fd0f771fd0c6492e6d47175a4503f6b407
Zlatan <zlajox@gmail.com>
2017-05-30 10:27:28 +0200

    Added dialog for new category

--- buspass-ws-master/src/main/resources/static/html/js/homescript.js
+++ buspass-ws-master/src/main/resources/static/html/js/homescript.js
@@ -11,6 +11,23 @@ username = pom[1];
 
 $(document).ready(function(){
    $('#usernamePlaceholder').html('<span class="glyphicon glyphicon-user"></span> ' + username);
+   if(type === "manager")
+        managerOnload();
+
+   $( function() {
+       $("#dialog").dialog({
+            autoOpen: false,
+            show: {
+                effect: "blind",
+                duration: 500
+                },
+            hide: {
+                effect: "explode",
+                duration: 500
+                }
+       });
+
+   });
 });
 
 function logout(){
@@ -42,4 +59,28 @@ function profile(){
 Date.prototype.subtractdHours= function(h){
     this.setHours(this.getHours()-h);
     return this;
+}
+
+//Manager things
+
+function managerOnload(){
+    $('#center').html('');
+    $('#center').html('<table id="centerTable"></table>');
+
+    $('#centerTable').append('<tr><td><h5>Update Buyer Categories </h5></td><td>&nbsp;<button>Update</button></td></tr>');
+    $('#centerTable').append('<tr><td><h5>Update Item Categories </h5></td><td>&nbsp;<button onclick="itemCategories()">Update</button></td></tr>');
+    $('#centerTable').append('<tr><td><h5>Update Action Events </h5></td><td>&nbsp;<button>Update</button></td></tr>');
+}
+
+function itemCategories(){
+    $('#center').html('');
+    $('#center').append('<br>');
+    $('#center').append('<h5>Add new item category &nbsp; <input type="button" onclick="aa()" id="addNewCategory" value="add"></h5>');
+    $('#center').append('<h4>Preview of all item categories: </h4>');
+    //TODO ajax call to list all categories
+    //$('#center').append('<div id="dialog" title="Modify artist"><p>Name</p><input name="name" type="text" id="Dname"><br><p>Surname</p><input name="surname" type="text" id="Dsurname"><br><p>Hit</p><input name="hit" type="text" id="Dhit"><br><br><button id="dialogModify">Save</button></div>');
+}
+
+function aa(){
+     $("#dialog").dialog("open");
 }
