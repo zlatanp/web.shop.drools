347c645d251c05f2d933e9346fab2460f3bee699
Zlatan <zlajox@gmail.com>
2017-07-09 13:45:54 +0200

    Buyer done.

--- buspass-ws-master/src/main/resources/static/html/js/homescript.js
+++ buspass-ws-master/src/main/resources/static/html/js/homescript.js
@@ -157,12 +157,19 @@ function profile(){
         dataType: 'json',
         data: {username : username},
         success: function(data){
+            console.log(data);
             $('#center').append('<h3>Name: ' + data.name +  '</h3><br>');
             $('#center').append('<h3>Surname: ' + data.surname +  '</h3><br>');
             $('#center').append('<h6>Username: ' + data.username +  '</h6><br>');
             $('#center').append('<h6>Password: ' + data.password +  '</h6><br>');
             $('#center').append('<h6>Type: ' + data.type +  '</h6><br>');
-            $('#center').append('<h6>Buyer Profile: ' + data.buyerProfile +  '</h6><br>');
+            if(data.type == "BUYER") {
+                var bp = data.buyerProfile;
+                $('#center').append('<h6>Buyer Profile:</h6><br>');
+                $('#center').append('<h6>Reward Points : ' + bp.rewardPoints + '</h6><br>');
+                var cat = bp.category;
+                $('#center').append('<h6>Category : ' + cat.name + '</h6><br>');
+            }
             $('#center').append('<h6>ID: ' + data.id +  '</h6><br>');
             $('#center').append('<h6>Date of registration: ' + String(new Date(data.dateOfRegistration).subtractdHours(2)) +  '</h6><br>');
         },
